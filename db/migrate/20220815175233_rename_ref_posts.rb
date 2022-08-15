c0b7293f49efc135fcb5ffc111884ac50a7ff5c0
medsonmoombe <medsonmoombe21@gmail.com>
2022-08-15 20:01:10 +0200

    Run linter

--- db/migrate/20220815175233_rename_ref_posts.rb
+++ db/migrate/20220815175233_rename_ref_posts.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class RenameRefPosts < ActiveRecord::Migration[7.0]
   def change
     rename_column :posts, :user_id, :author_id
-    #Ex:- rename_column("admin_users", "pasword","hashed_pasword")
+    # Ex:- rename_column("admin_users", "pasword","hashed_pasword")
   end
 end
--- db/migrate/20220815175512_rename_ref_comments.rb
+++ db/migrate/20220815175512_rename_ref_comments.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RenameRefComments < ActiveRecord::Migration[7.0]
   def change
     rename_column :comments, :user_id, :author_id
