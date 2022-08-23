bb03fb6569dc8466769664b45a9d11c60f3ec0de
medsonmoombe <medsonmoombe21@gmail.com>
2022-08-23 11:05:55 +0200

    Refacotered the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'users/index'
-  get 'users/show'
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
