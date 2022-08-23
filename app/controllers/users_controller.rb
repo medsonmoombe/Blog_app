5ed2de5c6b9908ef14c1b67f6dde253d39774b83
medsonmoombe <medsonmoombe21@gmail.com>
2022-08-24 01:14:29 +0200

    Add the views for the users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   def index
-    @users = User.order(:name) 
+    @users = User.order(:name)
   end
 
   def show
-    @user = find(params[:id])
+    @user = User.find(params[:id])
     @recent_posts = @user.recent_posts
   end
 end
