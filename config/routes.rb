58e74c111c18316812d9c7b966699acef49d60c7
medsonmoombe <medsonmoombe21@gmail.com>
2022-08-23 10:35:24 +0200

    Add the controllers and the views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
+  get 'users/index'
+  get 'users/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
