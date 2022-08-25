40bb93fb49b5bd6b29b9dd5a388334d98c46da69
medsonmoombe <medsonmoombe21@gmail.com>
2022-08-25 10:53:59 +0200

    Add the methods in the posts controller and updated the routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,23 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @posts = Post.new(posts_params)
+    if @post.save
+      redirect_to user_post_path(id: @post.id, user_id: @post.author_id), notice: 'Post created succesfully!'
+    else
+      render :new, alert: 'Post could not be created an Error occurred!'
+    end
+  end
+
+  private
+
+  def posts_params
+    params.require(:post).permit(:title, :text, :author_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'likes/create'
-  get 'comments/new'
-  get 'comments/create'
   root 'users#index'
 
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[index show new create]
+  end
+
+  resources :posts do
+    resources :comments, only: %i[create]
+    resources :likes, only: %i[create]
   end
 end
